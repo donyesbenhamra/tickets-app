af9b6c879bade99a5f31f537d4ac0132fc5a2efd
ZeinebLahbacha <zeineb.lahbacha@esen.tn>
2025-12-10 21:26:21 +0100

    Add user functionalities

--- tickets-app/src/main/java/com/example/demo/ticketsapp/model/Utilisateur.java
+++ tickets-app/src/main/java/com/example/demo/ticketsapp/model/Utilisateur.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 @Entity
 public class Utilisateur {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -11,5 +12,14 @@ public class Utilisateur {
     private String nom;
     private String email;
 
-    // TODO: Ajouter les constructeurs, getters et setters
+    public Utilisateur() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
